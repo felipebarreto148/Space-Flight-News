018934164f9f976154b0d16f581450b4f74c2d79
Felipe Barreto <felipebarreto148@gmail.com>
2022-06-02 18:57:11 -0300

    feat: start project and install composition-api

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import "@/composition-api";
+
 import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
@@ -7,7 +9,7 @@ import store from "./store";
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+	router,
+	store,
+	render: (h) => h(App),
 }).$mount("#app");
