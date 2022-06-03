d05c2a73d6b8c2ba4f9412e368dd79b7153f8720
Felipe Barreto <felipebarreto148@gmail.com>
2022-06-02 21:28:06 -0300

    feat: Sort by component created

---
+++ src/components/Inputs/Search/index.vue
@@ -0,0 +1,13 @@
+<template>
+	<input class="search__field field" type="text" placeholder="Search" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({});
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/_variables.scss";
+</style>
