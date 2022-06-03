e243f24f1c1a88034ef008f5842e7a7e61d8927e
Felipe Barreto <felipebarreto148@gmail.com>
2022-06-03 15:31:12 -0300

    feat: Called methods in components

--- src/components/Inputs/Search/index.vue
+++ src/components/Inputs/Search/index.vue
@@ -1,11 +1,40 @@
 <template>
-  <input class="search__field field" type="text" placeholder="Search" />
+	<input
+		class="search__field field"
+		type="text"
+		placeholder="Search"
+		v-model="state.search"
+	/>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@vue/composition-api";
+import { defineComponent, reactive, watch } from "@vue/composition-api";
 
-export default defineComponent({});
+export default defineComponent({
+	setup(_, { root }) {
+		const $store = root.$store;
+		const state = reactive({
+			search: "",
+		});
+
+		watch(
+			() => state.search,
+			(value) => {
+				if (value.length) {
+					$store.commit("resetStart");
+					$store.commit("setSearch", value);
+					$store.dispatch("getArticlesByTitle");
+				} else {
+          $store.dispatch("getArticles");
+        }
+			}
+		);
+
+		return {
+			state,
+		};
+	},
+});
 </script>
 
 <style lang="scss" scoped>
--- src/components/Selects/SortBy/index.vue
+++ src/components/Selects/SortBy/index.vue
@@ -1,21 +1,47 @@
 <template>
-  <select class="sortby field" name="sortby" id="sortby">
-    <option value="0" disabled selected>Sort</option>
-    <option value="2">Mais Antigas</option>
-    <option value="1">Mais Novas</option>
-  </select>
+	<select
+		class="sortby field"
+		name="sortby"
+		id="sortby"
+		v-model="state.sortby"
+		@change="() => sortBy()"
+	>
+		<option value="" selected disabled hidden>Sort</option>
+		<option value="publishedAt">Mais Antigas</option>
+		<option value="newer">Mais Novas</option>
+	</select>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@vue/composition-api";
+import { defineComponent, reactive } from "@vue/composition-api";
 
-export default defineComponent({});
+export default defineComponent({
+	setup(_, { root }) {
+		const $store = root.$store;
+		const state = reactive({
+			sortby: "",
+		});
+
+		async function sortBy() {
+			if (state.sortby == "newer") {
+				await $store.dispatch("getArticles");
+			} else {
+				await $store.commit("setSort", state.sortby);
+				await $store.dispatch("getArticlesBySort");
+			}
+		}
+		return {
+			state,
+			sortBy,
+		};
+	},
+});
 </script>
 
 <style lang="scss" scoped>
 @import "@/scss/_variables.scss";
 
 .sortby {
-  min-width: 120px;
+	min-width: 120px;
 }
 </style>
